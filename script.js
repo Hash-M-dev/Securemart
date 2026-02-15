c5fb9a1ae93c956403d84919702566a373c3c884
Hash-M-dev <hashir.mahmood.qtz@gmail.com>
2026-02-15 18:22:39 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// Dark mode toggle
+const toggle = document.getElementById("themeToggle");
+
+toggle.addEventListener("click", () => {
+    document.body.classList.toggle("dark");
+
+    toggle.textContent =
+        document.body.classList.contains("dark") ? "☀️" : "🌙";
+});
